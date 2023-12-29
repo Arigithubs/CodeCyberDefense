b5c557086af9b06ab2844164a68c99502f0b285d
Arigithubs <arimail@duck.com>
2023-12-28 23:42:14 -0500

    Add content to dynamically created game elements

--- js/game.js
+++ js/game.js
@@ -1,9 +1,10 @@
 // Get the game container element
 const gameContainer = document.getElementById('game-container');
 
-// Create and append game elements dynamically
+// Create and append game elements dynamically with content
 for (let i = 0; i < 100; i++) {  // 10 columns x 10 rows for a 10x10 grid
     const gameElement = document.createElement('div');
     gameElement.classList.add('game-element');
+    gameElement.textContent = i + 1;  // Add some content, e.g., numbers
     gameContainer.appendChild(gameElement);
 }
