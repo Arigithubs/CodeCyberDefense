4fcd67f4e7faf4ac300b761d7346ffaba4e4ce1d
Arigithubs <arimail@duck.com>
2023-12-28 23:40:37 -0500

    Add dynamic game elements to game container

--- js/game.js
+++ js/game.js
@@ -1,1 +1,9 @@
-console.log('CodeCyberDefense game.js is linked!');
+// Get the game container element
+const gameContainer = document.getElementById('game-container');
+
+// Create and append game elements dynamically
+for (let i = 0; i < 100; i++) {  // 10 columns x 10 rows for a 10x10 grid
+    const gameElement = document.createElement('div');
+    gameElement.classList.add('game-element');
+    gameContainer.appendChild(gameElement);
+}
